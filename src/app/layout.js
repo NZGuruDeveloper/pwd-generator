2cd8c490af61ed32fadb6dd6d093d8d1b7d707df
NZGuruDeveloper <hamed_hawwari@hotmail.com>
2023-12-28 15:49:20 +1300

    Vercel speed analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-jetbrains-mono' })
 
@@ -15,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetBrainsMono.className}>{children}<Analytics /></body>
+      <body className={jetBrainsMono.className}>{children}<Analytics /><SpeedInsights /></body>
     </html>
   )
 }
